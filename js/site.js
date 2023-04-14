9c7dbea62537032cc0ac98df1a9e489e134b9a92
Jamal Gibson <jamal.gibson@rogers.com>
2023-04-14 15:39:35 -0400

    Added app logic

--- js/site.js
+++ js/site.js
@@ -1,12 +1,46 @@
-function displayMessage() {
-  let msg = document.getElementById('message').value;
-  //alert(msg);
+function getValues() {
+  let userInput = document.getElementById('message').value;
 
-  Swal.fire(
-    {
-        backdrop: false,
-        title: 'App Name',
-        text: msg
-    }
-  );
+
+  let palindromeResults = checkForPalindrome(userInput);
+
+  displayResults(palindromeResults);
+}
+
+function checkForPalindrome(message) {
+  let results = '';
+
+  for (i = message.length - 1; i >=0; i--)
+  {
+    results += message[i];
+  }
+
+  if (results == message)
+  {
+    return [true, results];
+  }
+
+  else
+  {
+    return [false, results];
+  }
+}
+
+function displayResults(results) {
+
+
+  document.getElementById("msg").textContent = results[1];
+  
+  if (results[0] == true)
+  {
+    
+    document.getElementById("alert").classList.add("alert-success");
+    document.getElementById('resultsMessage').innerText = 'Success! Your string is a palindrome.'
+  }
+  else
+  {
+    document.getElementById("alert").classList.add("alert-danger");
+    document.getElementById("resultsMessage").innerText = "Failure! Your string is not a palindrome.";
+  }
+  document.getElementById("alert").classList.remove("d-none");
 }
